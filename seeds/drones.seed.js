92de06974f24ffe807e5a85eb24bebcd7893d77b
Cristina Chaparro <cricbau@gmail.com>
2023-02-13 17:09:00 +0100

    iteration 1 done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,19 @@
-// Iteration #1
+require("../db");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+const Drone = require("../models/Drone.model.js");
+
+async function insertData() {
+  try {
+    const response = await Drone.insertMany(drones);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+insertData();
